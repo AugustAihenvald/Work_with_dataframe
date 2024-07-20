e19c332978b4e884f4e226eeae6395fdda69a824
Anton <augustaihenvald@gmail.com>
2024-07-20 20:59:53 +0300

    added a function to convert to <one hote>

--- work_with_dataframe.py
+++ work_with_dataframe.py
@@ -5,4 +5,18 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
+def get_value(row):
+    binary_true, binary_false = 1, 0
+
+    if row['whoAmI'] == 'human':
+        row['robot'] = binary_false
+        row['human'] = binary_true
+    elif row['whoAmI'] == 'robot':
+        row['robot'] = binary_true
+        row['human'] = binary_false
+    return row
+
+data = data.apply(get_value, axis=1)
+del data['whoAmI']
+print(data)
 
